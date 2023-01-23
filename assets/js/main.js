fd484f309ff55ec20a936934709241e9b7720adf
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-23 21:11:33 +0100

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 let colorInput = document.getElementById("colorInput")
 let background = document.querySelector("body")
 let button = document.getElementById("button")
-
-button.addEventListener('click', () =>{
-    let inputValue = colorInput.value;
-    console.log(inputValue);
-    background.style.background=`${inputValue}`;
-})
+    button.addEventListener('click', () =>{
+        let inputValue = colorInput.value;
+        background.style.background=`${inputValue}`;
+    })
